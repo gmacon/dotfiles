35546eb12543318adfb4b9c29dc0a9348c2b7acf
George Macon <george.macon@gtri.gatech.edu>
2013-09-09 12:07:27 -0400

    Add dwm scripts

---
+++ status.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+while sleep 5; do
+	vpn=$(pgrep -lf openvpn | awk '{print $NF}' | sort -u | xargs)
+	tosleep=$((180 - $(xssstate -i) / 1000))
+	if [ $tosleep -le 30 ]; then
+		sleepwarning="Sleeping Soon! "
+	else
+		sleepwarning=""
+	fi
+	now=$(date '+%b %-d %-H:%M')
+	xsetroot -name "$sleepwarning[$vpn] $now"
+done
