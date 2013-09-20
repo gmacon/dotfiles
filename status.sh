1ca0a9f5661e5e361f51d81b66d7c3914361b8b0
George Macon <george.macon@gtri.gatech.edu>
2013-09-20 13:25:34 -0400

    Fancier screensaver handling

--- status.sh
+++ status.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
-while sleep 5; do
+sleepperiod=5
+while sleep $sleepperiod; do
 	vpn=$(pgrep -lf openvpn | awk '{print $NF}' | sort -u | xargs)
-	tosleep=$((180 - $(xssstate -i) / 1000))
-	if [ $tosleep -le 30 ]; then
-		sleepwarning="Sleeping Soon! "
-	else
-		sleepwarning=""
-	fi
+	sleepwarning=""
+	sleepperiod=5
+	case $(xssstate -s) in
+		on)
+			sxlock -f "-*-terminus-medium-*-normal--32-*-*-*-*-*-*-u";;
+		off)
+			tosleep=$(($(xssstate -t) / 1000))
+			if [ $tosleep -le 30 ]; then
+				sleepwarning="Sleeping in $tosleep! "
+				sleepperiod=1
+			fi;;
+	esac
 	now=$(date '+%b %-d %-H:%M')
 	project=$(timecard --current)
 	xsetroot -name "$sleepwarning{$project} [$vpn] $now"
