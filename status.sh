1405ab9a605b7f84e3e6aea4d3fcd103fd26b658
George Macon <george.macon@gtri.gatech.edu>
2015-01-08 10:37:38 -0500

    Display swaprate

--- status.sh
+++ status.sh
@@ -13,8 +13,9 @@ while sleep $sleepperiod; do
 				sleepperiod=1
 			fi;;
 	esac
+	swaprate=$(vmstat -w | tail -n1 | awk '{print $7, $8}')
 	now=$(date '+%a %b %-d %-H:%M')
 	utcnow=$(date -u '+%-H:%MZ')
 	project=$(timecard --current)
-	xsetroot -name "$sleepwarning{$project} $now $utcnow"
+	xsetroot -name "$sleepwarning{$project} swaprate ${swaprate} $now $utcnow"
 done
