e3b8ed263aa6ec0f2ff466549df6acd22c3c1d15
George Macon <george.macon@gmail.com>
2018-06-02 12:45:05 -0400

    Add crates.io search for qutebrowser

--- qutebrowser.py
+++ qutebrowser.py
@@ -6,6 +6,7 @@ c.url.searchengines = {
     'aw': 'https://wiki.archlinux.org/index.php?search={}',
     'aur': 'https://aur.archlinux.org/packages/?O=0&K={}',
     'pypi': 'https://pypi.org/search/?q={}',
+    'crate': 'https://crates.io/search?q={}',
 }
 
 c.url.default_page = 'about:blank'
