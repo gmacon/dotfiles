e274069f5e13da8d253b246507d424f2b2a1d5b3
George Macon <George.Macon@gtri.gatech.edu>
2019-09-13 17:13:04 -0400

    Add Doom Emacs config

---
+++ doom/config.el
@@ -0,0 +1,6 @@
+;;; .doom.d/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here
+
+(setq doom-font (font-spec :family "Fira Code" :size 12))
+(after! smartparens (smartparens-global-mode -1))
