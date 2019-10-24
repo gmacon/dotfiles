e2d07ba85e9066750f3ab153e612f65b357bd6bb
George Macon <George.Macon@gtri.gatech.edu>
2019-10-24 12:09:55 -0400

    emacs: use default font

--- doom/config.el
+++ doom/config.el
@@ -2,6 +2,5 @@
 
 ;; Place your private configuration here
 
-(setq doom-font (font-spec :family "DejaVu Sans Mono" :size 12))
 (after! smartparens (smartparens-global-mode -1))
 (add-hook! 'before-save-hook 'doom/delete-trailing-newlines 'delete-trailing-whitespace)
