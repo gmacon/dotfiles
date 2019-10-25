a9335d58b579585c0d39172799945bb679302ccd
George Macon <George.Macon@gtri.gatech.edu>
2019-10-25 10:19:56 -0400

    Bind next/prev error

--- doom/config.el
+++ doom/config.el
@@ -3,3 +3,10 @@
 ;; Place your private configuration here
 
 (after! smartparens (smartparens-global-mode -1))
+
+(map! :leader
+      (:prefix-map ("l" . "jump")
+        :desc "Next error" "n" #'flycheck-next-error
+        :desc "Prev error" "p" #'flycheck-previous-error
+        )
+      )
