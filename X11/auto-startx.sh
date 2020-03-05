2050944a9af8b9b52f1fdd6f91064989134cb841
George Macon <George.Macon@gtri.gatech.edu>
2020-03-05 16:14:27 -0500

    Don't start X11 from inside tmux

--- X11/auto-startx.sh
+++ X11/auto-startx.sh
@@ -1,3 +1,3 @@
-if [ -z "$DISPLAY" ] && [ -n "$XDG_VTNR" ] && [ "$XDG_VTNR" -eq 1 ]; then
+if [ -z "$DISPLAY" ] && [ -n "$XDG_VTNR" ] && [ "$XDG_VTNR" -eq 1 ] && [ "$TERM" != "screen" ]; then
   exec startx
 fi
