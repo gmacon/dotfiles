006d0ba3f0f751f3d46dbd0c0c41aab1bcda2fd4
George Macon <george.macon@gtri.gatech.edu>
2023-01-04 08:24:38 -0500

    [emacs] Update emacs config
    This is a reworking based on Bryan's config, including the switch to
    pgtk nightlies. The weird infinite recursion I was seeing with my
    previous config is gone, but we don't know why.

--- emacs/init.el
+++ emacs/init.el
@@ -1,312 +1,244 @@
-                                        ; Don't resize frame when font size changes
-(setq frame-inhibit-implied-resize t
-      frame-resize-pixelwise t
-                                        ; This is inferred as just "Macon", thanks, Microsoft
-      user-full-name "George Macon"
-      gc-cons-threshold (* 100 (* 1024 1024)) ; 100 MiB
-      read-process-output-max (* 1024 1024)) ; 1 MiB
-                                        ; Hide unneeded UI elements
-(push '(menu-bar-lines . 0) default-frame-alist)
-(push '(tool-bar-lines . 0) default-frame-alist)
-(push '(vertical-scroll-bars) default-frame-alist)
+;;; package --- Summary
+;;; Commentary:
+;;; Code:
+;; -*- lexical-binding: t -*-
 
-                                        ; Make custom-file a separate file not tracked by version control.
-(setq custom-file "~/.config/emacs/custom.el")
-(load custom-file 'noerror)
+(defvar gam-default-leader-key "<SPC>")
 
-                                        ; Fonts
-(defvar gam/font-family "Fira Code")
-(defvar gam/font-size 120)
-(set-face-attribute 'default nil
-  :family gam/font-family
-  :height gam/font-size
-  :width 'normal
-  :weight 'normal)
+(set-face-attribute 'default nil :family "Fira Code")
+(set-face-attribute 'line-number-current-line nil :family "Fira Code")
 
-(set-face-attribute 'line-number-current-line nil
-  :family gam/font-family
-  :height gam/font-size
-  :width 'expanded
-  :weight 'normal
-  :inverse-video nil)
+(setq default-frame-alist
+      '((menu-bar-lines . 0)
+        (tool-bar-lines . 0) 
+        (vertical-scroll-bars . nil)))
 
-                                        ; Save backups in temp file
-(setq backup-directory-alist `((".*" . ,temporary-file-directory))
-      auto-save-file-name-transforms `((".*" ,temporary-file-directory t)))
+(use-package general)
 
-                                        ; Avoid bad tramp-zsh interaction
-                                        ; Note: this depends on the zsh/zshrc handling
-(setq tramp-terminal-type "tramp"
-      tramp-default-method "ssh")
+(use-package emacs
+  :custom (custom-file null-device "disable customizations")
+          (initial-scratch-message "" "disable the scratch message")
+          (inhibit-startup-message t "disable the startup screen")
+          (scroll-conservatively 101 "move window one line at a time when point approaches edge")
+          (scroll-margin 5 "start scrolling 5 lines from edge")
+          (visible-bell t "Audible bell is cancer, but visible bell works okay")
+          (ad-redefinition-action 'accept "Tell emacs we're okay with functions being given advice")
+          (select-enable-clipboard t "copy actions copy to clipboard")
+          (select-enable-primary t "copy actions also copy to primary")
+          (mouse-drag-copy-region t "highlighting a section causes it to get copied (linux default behavior)")
+          (backup-directory-alist `((".*" . ,temporary-file-directory)) "Move backups to a temporary dir")
+          (auto-save-file-name-transforms `((".*" ,temporary-file-directory t)) "Move auto saves to a temporary dir")
+          (read-process-output-max (* 1024 1024) "Up amount of output we can read from a subprocess buffer; should improve LSP")
+          (gc-cons-threshold 100000000 "Increase garbage collection-threshold")
 
-                                        ; Grab $PATH from shell configuration
-(use-package exec-path-from-shell
-  :init (setq exec-path-from-shell-check-startup-files nil
-              exec-path-from-shell-shell-name "zsh")
-  :config
-  (add-to-list 'exec-path-from-shell-variables "LC_COLORSCHEME_VARIANT")
-  (add-to-list 'exec-path-from-shell-variables "CONDA_EXE")
-  (exec-path-from-shell-initialize))
+  :init
+    (defun gam-prog-mode-setup ()
+      (show-paren-mode 1)            ; highlight matching brackets
+      (global-hl-line-mode 1)        ; highlight the active line
+      (display-line-numbers-mode)    ; Show line numbers
+      (hs-minor-mode))               ; Add hide-show
 
-                                        ; Evil mode
-(use-package undo-tree
-  :init (setq undo-tree-history-directory-alist '(("." . "~/.cache/emacs/undo-tree")))
-  :config (global-undo-tree-mode))
-(use-package evil-leader
-  :init (setq evil-want-keybinding nil)
-    :config
-    (evil-leader/set-leader "<SPC>")
-    (global-evil-leader-mode)
-    (evil-leader/set-key
-      "." 'find-file
-      "b" 'switch-to-buffer
-      "w" 'toggle-truncate-lines))
-(use-package evil
-    :demand t
-    :after evil-leader
-    :defer 0.1
-    :init
-    (setq evil-search-module 'evil-search
-          evil-ex-substitute-global t)
-    :config
-    (evil-global-set-key 'normal (kbd "g r") 'xref-find-references)
-    (evil-set-undo-system 'undo-tree)
-    (evil-mode 1))
+    (defun gam-after-init-hook ()
+      (defalias 'yes-or-no-p 'y-or-n-p)  ; I don't ever want to type out "yes" or "no" - even if it is important
+      (global-unset-key (kbd "C-h h"))   ; I have *never* wanted to see the hello file.
 
-(use-package evil-surround
-    :after evil
-    :config (global-evil-surround-mode 1))
+      (setq-default
+        fill-column 80                            ; in fill-mode, what column do we wrap at?
+        truncate-lines t                          ; disable line wrapping
+        indent-tabs-mode nil                      ; use spaces over tabs everywhere
+        tab-width 4                               ; but when encountering a tab, how large is it?
+        ))
 
-(use-package evil-collection
-  :after evil
-  :config (evil-collection-init))
+    :general (
+       :states 'normal
+       :prefix gam-default-leader-key
+       "." #'find-file
+       gam-default-leader-key #'project-find-file)
 
-                                        ; Sorting and Filtering
-(use-package selectrum
-  :config (selectrum-mode 1))
-(use-package prescient)
+  :hook ((prog-mode . gam-prog-mode-setup)
+         (after-init . gam-after-init-hook)
+         (before-save . 'whitespace-cleanup)))
 
-(use-package selectrum-prescient
-  :config
-  (selectrum-prescient-mode)
-  (prescient-persist-mode))
+(use-package textsize
+  :config (textsize-mode))
 
-                                        ; Completion
-(use-package company
-  :delight company-mode
-  :config
-    (setq company-tooltip-limit 20
-          company-tooltip-align-annotations t)
-    )
-
-                                        ; Theme
-(defun gam/macos-responsive-theme (appearance)
-  "Change theme based on system APPEARANCE."
-  (mapc #'disable-theme custom-enabled-themes)
-  (pcase appearance
-    ('light (load-theme 'doom-solarized-light))
-    ('dark (load-theme 'doom-solarized-dark))))
 (use-package doom-themes
-  :init (setq doom-solarized-dark-brighter-comments t)
+  :defer nil
   :config
-  (if (memq window-system '(mac ns))
-      (add-hook 'ns-system-appearance-change-functions #'gam/macos-responsive-theme)
-    (load-theme 'doom-solarized-light))
-  (doom-themes-visual-bell-config)
-  (doom-themes-org-config))
+      (load-theme 'doom-solarized-light t)
+      (doom-themes-visual-bell-config)
+      (doom-themes-org-config))
+
 (use-package doom-modeline
   :config (doom-modeline-mode))
 
-                                        ; Folding
-(use-package origami
-  :hook ((prog-mode . 'origami-mode)))
+(use-package evil
+    :custom (evil-undo-system 'undo-redo)
+            (evil-want-keybinding nil)
+            (evil-echo-state nil)
+            (evil-want-integration t)
+            :config (evil-mode 1))
 
-                                        ; Display available binds
-(use-package which-key
-  :init (which-key-mode))
+(use-package evil-collection
+    :after (evil)
+    :config (evil-collection-init '(magit magit-todos consult)))
 
-                                        ; Projects
-(use-package perspective
-  :init (setq persp-show-modestring nil
-              persp-suppress-no-prefix-key-warning t)
-  :config
-    (evil-leader/set-key "B" 'persp-switch-to-buffer)
-    (persp-mode))
+(use-package evil-surround
+  :after evil
+  :config (global-evil-surround-mode 1))
 
-(use-package persp-projectile
-    :commands (projectile-persp-switch-project)
-    :init (evil-leader/set-key "p" 'projectile-persp-switch-project))
+(use-package eldoc-box
+  :hook (prog-mode . eldoc-box-hover-mode))
 
-(use-package projectile
-  :delight projectile-mode
-  :commands (projectile-switch-project projectile-find-file projectile-mode)
-  :after evil-leader
-  :init
-    (setq projectile-completion-system 'default
-          projectile-require-project-root nil
-          projectile-git-command "fd . --hidden --exclude=.git --print0 --color never"
-          projectile-indexing-method 'alien
-          projectile-project-search-path '("~/code"))
-    (evil-leader/set-key
-      evil-leader/leader 'projectile-find-file
-      "," 'projectile-switch-to-buffer)
-  :config
-  (projectile-mode))
+(use-package corfu
+  :custom (corfu-auto t)
+  :init (global-corfu-mode))
 
-                                        ; Text Mode Basics
-(add-hook 'text-mode-hook 'flyspell-mode)
-(add-hook 'text-mode-hook 'display-line-numbers-mode)
-
-                                        ; Programming Mode Basics
-(add-hook 'prog-mode-hook (function(lambda ()
-  (show-paren-mode 1)
-  (global-hl-line-mode 1)
-  (display-line-numbers-mode)
-  (column-number-mode)
-  (company-mode)))
-)
-(setq initial-scratch-message ""                     ; disable the scratch message
-      inhibit-startup-message t                      ; disable the startup screen
-      scroll-conservatively 101                      ; move window one line at a time when point approaches edge
-      scroll-margin 5                                ; start scrolling 5 lines from edge
-      visible-bell t                                 ; Audible bell is cancer, but visible bell works okay
-      ad-redefinition-action 'accept                 ; Tell emacs we're okay with functions being given advice
-      vc-follow-symlinks t                           ; Follow symlinks to vcs controlled files
-      mouse-drag-copy-region t                       ; highlighting a section causes it to get copied (linux default behavior)
-)
-(setq-default fill-column 80                         ; in fill-mode, what column do we wrap at?
-              truncate-lines t                       ; disable line wrapping
-              indent-tabs-mode nil                   ; use spaces over tabs everywhere
-              tab-width 4                            ; but when encountering a tab, how large is it?
-              tab-stop-list (number-sequence 3 120 2); and what are the tabstop points when shifting?
-              require-final-newline t                ; the last line isn't complete without its end-of-line marker
-)
-
-                                        ; Git
-(use-package git-gutter
-  :hook
-  ((prog-mode . 'git-gutter-mode)
-   (text-mode . 'git-gutter-mode))
-  :init (setq git-gutter:update-interval 2))
 (use-package magit
-  :commands (magit-status)
-  :hook ((after-save . 'magit-after-save-refresh-status)
-         (git-commit-setup . 'git-commit-turn-on-flyspell))
-  :defer 5
-  :init
-    (evil-leader/set-key "g" 'magit)
-    (setq magit-popup-show-common-commands nil
-          magit-display-buffer-function 'magit-display-buffer-same-window-except-diff-v1
-          git-commit-fill-column 72
-          git-commit-summary-max-length 50))
+    :hook (after-save . magit-after-save-refresh-status)
+    :general (:states 'normal :prefix gam-default-leader-key
+              "g" 'magit)
+    :custom (magit-popup-show-common-commands nil)
+            (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1)
+            (git-commit-fill-column 72)
+            (git-commit-summary-max-length 50))
 
-                                        ; Direnv
+(use-package git-gutter
+  :hook (prog-mode . git-gutter-mode)
+  :custom (git-gutter:update-interval 2))
+
+(use-package git-gutter-fringe
+  :config
+  (define-fringe-bitmap 'git-gutter-fr:added [224] nil nil '(center repeated))
+  (define-fringe-bitmap 'git-gutter-fr:modified [224] nil nil '(center repeated))
+  (define-fringe-bitmap 'git-gutter-fr:deleted [128 192 224 240] nil nil 'bottom))
+
 (use-package direnv
   :config (direnv-mode))
 
-                                        ; Editorconfig
-(use-package editorconfig
-  :config (editorconfig-mode 1))
-
-                                        ; Highlight TODO notes
 (use-package fic-mode
-  :commands (fic-mode)
-  :init (setq fic-highlighted-words '("FIXME" "TODO" "BUG" "NOTE" "XXX"))
-  :hook ((prog-mode . 'fic-mode)))
+    :custom (fic-highlighted-words '("FIXME" "TODO" "BUG" "NOTE" "XXX"))
+    :hook (prog-mode . fic-mode))
 
-                                        ; Whitespace cleanup
-(use-package ws-butler
-  :hook ((prog-mode . 'ws-butler-mode)))
+(use-package which-key
+  :config (which-key-mode 1))
 
-                                        ; Separedit
-(use-package separedit
-  :config (setq separedit-default-mode 'markdown-mode
-                separedit-preserve-string-indentation t
-                separedit-continue-fill-column t
-                separedit-remove-trailing-spaces-in-comment t)
-  :init (evil-leader/set-key "e" 'separedit))
+(use-package yasnippet
+  :hook (prog-mode . yas-minor-mode))
 
-                                        ; Language Servers
-(use-package yasnippet)
-(use-package markdown-mode)
 (use-package eglot
-  :hook
-  ((python-mode . 'eglot-ensure)
-   (rust-mode . 'eglot-ensure)
-   (nix-mode . 'eglot-ensure))
+  :after transient
+  :custom (eglot-extend-to-xref t)
+  :general (:states 'normal
+                  :prefix gam-default-leader-key
+                    "l a" 'eglot-code-actions
+                    "l h" 'eldoc
+                    "l l" 'eglot
+                    "l q" 'eglot-shutdown
+                    "l Q" 'eglot-reconnect
+                    "l r" 'eglot-rename
+                    "l =" 'eglot-format)
+  :hook ((python-mode . eglot-ensure)
+         (rust-mode . eglot-ensure)
+         (nix-mode . eglot-ensure)))
+
+(use-package sideline
+  :hook (flymake-mode . sideline-mode))
+
+(use-package sideline-flymake
+  :custom (sideline-flymake-display-errors-whole-line 'line)
+          (sideline-backends-right '((sideline-flymake . up))))
+
+(use-package consult
+  :general (:states 'normal :prefix gam-default-leader-key
+            "/" 'consult-ripgrep)
+  :custom (register-preview-delay 0)
+          (register-preview-function #'consult-register-format)
+          (xref-show-xrefs-function #'consult-xref)
+          (xref-show-definitions-function #'consult-xref)
   :init
-  (setq eglot-extend-to-xref t)
+    (advice-add #'register-preview :override #'consult-register-preview)
   :config
-  (evil-leader/set-key
-    "l a" 'eglot-code-actions
-    "l h" 'eldoc
-    "l l" 'eglot
-    "l q" 'eglot-shutdown
-    "l Q" 'eglot-reconnect
-    "l r" 'eglot-rename
-    "l =" 'eglot-format))
+    (consult-customize
+     consult-theme
+     :preview-key '(:debounce 0.2 any)
+     consult-ripgrep consult-git-grep consult-grep
+     consult-bookmark consult-recent-file consult-xref
+     consult--source-recent-file consult--source-project-recent-file consult--source-bookmark
+     :preview-key (kbd "M-.")))
 
-                                        ; Python
-(use-package cython-mode)
-(add-hook
- 'python-mode-hook
- (lambda ()
-   (add-hook 'before-save-hook 'eglot-format-buffer nil t)))
+(use-package consult-project-extra)
 
-                                        ; YAML
-(use-package yaml-mode
-  :mode ("\\.yaml'" "\\.yml'"))
+(use-package tabspaces
+  :hook (after-init . tabspaces-mode)
+  :custom (tabspaces-use-filtered-buffers-as-default t)
+          (tab-bar-show nil)
+  :commands (tabspaces-create-workspace
+             tabspaces-create-new-project-and-workspace
+             tabspaces-open-existing-project-and-workspace
+             tabspaces-switch-workspace)
+  :general (
+            :states 'normal
+    :prefix gam-default-leader-key
+    "p" #'tabspaces-open-or-create-project-and-workspace))
 
-                                        ; Org Mode
-(setq org-agenda-files '("~/org/"))
+(use-package vertico
+   :init (vertico-mode))
 
-                                        ; Salt
-(use-package salt-mode
-  :mode ("\\.sls'"))
+(use-package orderless
+  :custom (completion-styles '(orderless basic))
+          (completion-category-defaults nil)
+          (completion-category-overrides '((file (styles partial-completion)))))
 
-                                        ; The World Wide Web
-(use-package web-mode
-  :mode ("\\.jsx?$" "\\.tsx?$" "\\.html$" "\\.css$")
-  :init
-    (setq web-mode-markup-indent-offset 2
-          web-mode-code-indent-offset 2
-          web-mode-css-indent-offset 2
-          web-mode-attr-indent-offset 2
-          web-mode-enable-css-colorization t
-          web-mode-enable-current-column-highlight t
-          web-mode-enable-auto-quoting nil
-    ))
+(use-package marginalia
+  :general (:map minibuffer-local-map "M-A" 'marginalia-cycle)
+  :init (marginalia-mode))
 
-                                        ; Caddyfile
+;; languages
 (use-package caddyfile-mode
   :mode ("Caddyfile'"))
 
-                                        ; Dockerfile
+(use-package cython-mode)
+
 (use-package dockerfile-mode
   :mode ("Dockerfile"))
 
-                                        ; Rust
-(use-package rust-mode
-  :mode ("\\.rs$")
-  :hook ((rust-mode . (lambda ()
-                            (add-hook 'before-save-hook 'eglot-format-buffer nil t)))))
-
-                                        ; Julia
-(use-package julia-mode
-  :mode ("\\.jl$"))
-
-                                        ; DOT graph language
 (use-package graphviz-dot-mode
   :mode ("\\.dot$"))
 
-                                        ; Nix
-(use-package nix-mode
-  :mode ("\\.nix$"))
-
-                                        ; Packer (HCL)
 (use-package hcl-mode
   :mode ("\\.hcl$"))
 
-                                        ; Go
-(use-package go-mode
-  :mode ("\\.go$"))
+(use-package markdown-mode
+  :custom (markdown-command "pandoc")
+  :mode (("\\.md'" . gfm-mode)))
+
+(use-package nix-mode
+  :hook (nix-mode . (lambda () (add-hook 'before-save-hook 'eglot-format-buffer nil t))))
+
+(use-package python
+  ;; set ensure nil to use packaged version of python.el
+  ;; rather than grabbing from elpa
+  :ensure nil
+  :mode ("\\.py\\'" . python-mode)
+  :interpreter ("python" . python-mode)
+  :hook (python-mode . (lambda () (add-hook 'before-save-hook 'eglot-format-buffer nil t))))
+
+(use-package rust-mode :mode ("\\.rs'")
+  :hook (rust-mode . (lambda () (add-hook 'before-save-hook 'eglot-format-buffer nil t))))
+
+(use-package salt-mode
+  :mode ("\\.sls'"))
+
+(use-package yaml-mode :mode ("\\.yaml'" "\\.yml'"))
+
+(use-package web-mode
+  :custom
+  (web-mode-markup-indent-offset 2)
+          (web-mode-code-indent-offset 2)
+          (web-mode-css-indent-offset 2)
+          (web-mode-attr-indent-offset 2)
+          (web-mode-enable-css-colorization t)
+          (web-mode-enable-current-column-highlight t)
+          (web-mode-enable-auto-quoting nil))
+;;; emacs.el ends here
--- home.nix
+++ home.nix
@@ -180,6 +180,7 @@ in {
     enable = true;
     package = pkgs.emacsWithPackagesFromUsePackage {
       config = ./emacs/init.el;
+      package = pkgs.emacsPgtk;
       alwaysEnsure = true;
     };
   };
