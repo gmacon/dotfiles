1ee85a5fbeec87652af9e6ef84fa9ec065414869
George Macon <george.macon@gtri.gatech.edu>
2023-01-11 12:31:14 -0500

    [zsh] Caffeinate nix and home-manager on Darwin

--- darwin.nix
+++ darwin.nix
@@ -3,5 +3,9 @@
     "Library/Application Support/iTerm2/Scripts/autotheme.py".source =
       ./iterm2/autotheme.py;
   };
+  home.shellAliases = {
+    nix = "caffeinate nix";
+    home-manager = "caffeinate home-manager";
+  };
   programs.git.extraConfig.credential.helper = "osxkeychain";
 }
