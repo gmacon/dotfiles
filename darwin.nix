75dc550e9774a26b8372c3da307e2ad2d00e4365
George Macon <george.macon@gtri.gatech.edu>
2023-01-19 10:47:35 -0500

    [zsh] Include /usr/local/bin in PATH on Darwin

--- darwin.nix
+++ darwin.nix
@@ -3,6 +3,7 @@
     "Library/Application Support/iTerm2/Scripts/autotheme.py".source =
       ./iterm2/autotheme.py;
   };
+  home.sessionPath = [ "/usr/local/bin" ];
   home.shellAliases = {
     nix = "caffeinate nix";
     home-manager = "caffeinate home-manager";
