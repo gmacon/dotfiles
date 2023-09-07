4d852791d04149ac5f87e83fd2209f8c28b6ba61
George Macon <george.macon@gtri.gatech.edu>
2023-09-07 15:31:02 -0400

    [linux] Disable alacritty
    Since GPU things require NixOS specifically and don't work on Alien Nix.

--- linux.nix
+++ linux.nix
@@ -1,17 +1,3 @@
-{ config, pkgs, inputs, ... }: {
+{ pkgs, ... }: {
   home.packages = with pkgs; [ ncdu ];
-  programs.alacritty = {
-    enable = true;
-    settings = {
-      font.normal.family = "Fira Code Nerd Font";
-      font.size = 10.0;
-      draw_bold_text_with_bright_colors = true;
-      key_bindings = [
-        { key = "N"; mods = "Shift|Control"; action = "SpawnNewInstance"; }
-      ];
-      "import" = [
-        "${inputs.alacritty-theme-penumbra}/penumbra-light.yml"
-      ];
-    };
-  };
 }
