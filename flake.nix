1fc90048c7b3aa74377ab6f034a6c0fce181d858
George Macon <george.macon@gtri.gatech.edu>
2023-10-04 08:38:55 -0400

    [hm] Switch to nixos-23.05 channel

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Home Manager configuration of George Macon";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-23.05-darwin";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
     home-manager = {
       url = "github:nix-community/home-manager/release-23.05";
       inputs.nixpkgs.follows = "nixpkgs";
