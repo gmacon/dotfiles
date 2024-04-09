c2db4ae650483c9c7efab64ba2090ecb5ea10265
George Macon <george.macon@gtri.gatech.edu>
2024-04-09 10:26:02 -0400

    [wordle]: Ignore accents

--- nix/wordle.nix
+++ nix/wordle.nix
@@ -1,6 +1,26 @@
-{ writeShellApplication, scowl }: writeShellApplication {
+{ writeShellApplication, runCommand, python3, scowl }:
+let
+  wordleWords = runCommand "wordles" { buildInputs = [ python3 ]; } ''
+    python <<EOF
+    import re
+    import unicodedata
+
+    of = open("$out", "w")
+    for line in open("${scowl}/share/dict/words.txt", encoding="iso8859-1"):
+        word = (
+            unicodedata.normalize("NFKD", line)
+            .strip()
+            .encode("ascii", errors="ignore")
+            .decode("ascii")
+        )
+        if re.fullmatch("[a-z]{5}", word):
+            print(word, file=of)
+    EOF
+  '';
+in
+writeShellApplication {
   name = "wordle";
   text = ''
-    grep -E '^[a-z]{5}$' ${scowl}/share/dict/words.txt
+    cat ${wordleWords}
   '';
 }
