100a6165fffcdec4b8ddf3b2541b9ed9c5c8a811
George Macon <george@themacons.net>
2024-04-20 15:10:15 -0400

    [potassium] Move SSH to non-standard port

--- flake.nix
+++ flake.nix
@@ -142,6 +142,7 @@
               device = "/dev/disk/by-id/scsi-0DO_Volume_volume-nyc3-01";
               options = [ "discard" "nofail" "noatime" ];
             };
+            services.openssh.ports = [ 46409 ];
           }
           nixpkgsModule
           "${nixpkgs}/nixos/modules/virtualisation/digital-ocean-config.nix"
