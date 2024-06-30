9fc184f8a88939b9232573db0016ae7083d9f4f4
George Macon <george@themacons.net>
2024-06-30 19:59:46 -0400

    [argon] Disable lesspipe

--- argon/configuration.nix
+++ argon/configuration.nix
@@ -167,6 +167,8 @@
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
+  programs.less.lessopen = null;
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
