0001dc01049cad413d3f1b4cc8ce4fac80e34de1
George Macon <george@themacons.net>
2024-07-19 19:36:07 -0400

    [k] Enable fail2ban

--- potassium/configuration.nix
+++ potassium/configuration.nix
@@ -1,10 +1,15 @@
-{ config, ... }: {
+{ config, pkgs, ... }: {
   system.stateVersion = "23.11";
   fileSystems."/srv" = {
     device = "/dev/disk/by-id/scsi-0DO_Volume_volume-nyc3-01";
     options = [ "discard" "nofail" "noatime" ];
   };
   services.openssh.ports = [ 46409 ];
+  services.fail2ban = {
+    enable = true;
+    banaction = "iptables-ipset";
+    extraPackages = [ pkgs.ipset ];
+  };
 
   # Backups
   age.secrets.tarsnapKey.file = ../secrets/tarsnap-k.key.age;
