4914d8adae77bbe03485db533ea58298dcc4830a
George Macon <george@themacons.net>
2024-08-30 18:46:00 -0400

    [bridge-manager] 0.12.1 -> 0.12.2

--- nix/bridge-manager/default.nix
+++ nix/bridge-manager/default.nix
@@ -5,12 +5,12 @@
 }:
 buildGoModule rec {
   pname = "bridge-manager";
-  version = "0.12.1";
+  version = "0.12.2";
   src = fetchFromGitHub {
     owner = "beeper";
     repo = "bridge-manager";
     rev = "v${version}";
-    hash = "sha256-MAve7ZNS20XBJ7Qp3mehznli04+MS7RiVWW1txdf19U=";
+    hash = "sha256-Q8RgfkPw8KPkfORaPCwM18rNhzNm4UcH4hSdfYe4FZo=";
   };
   vendorHash = "sha256-uz4pao8Y/Sb3fffi9d0lbWQEUMohbthA6t6k6PfQz2M=";
   ldflags = [ "-X main.Tag=v${version}" ];
