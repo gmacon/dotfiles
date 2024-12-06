137012ca8a087fa71e8849fe69f3a10e358ee4c3
George Macon <george.macon@gtri.gatech.edu>
2024-12-06 11:47:16 -0500

    [emacs] Put Corfu in Tab-and-Go mode

--- home-manager/config/emacs/init.el
+++ home-manager/config/emacs/init.el
@@ -142,8 +142,22 @@
   :config (global-evil-surround-mode 1))
 
 (use-package corfu
-  :custom (tab-always-indent 'complete)
-  :init (global-corfu-mode))
+  ;; TAB-and-Go customizations
+  :custom
+  (corfu-auto t)
+  (corfu-cycle t)           ;; Enable cycling for `corfu-next/previous'
+  (corfu-preselect 'prompt) ;; Always preselect the prompt
+
+  ;; Use TAB for cycling, default is `corfu-complete'.
+  :bind
+  (:map corfu-map
+        ("TAB" . corfu-next)
+        ([tab] . corfu-next)
+        ("S-TAB" . corfu-previous)
+        ([backtab] . corfu-previous))
+
+  :init
+  (global-corfu-mode))
 
 (use-package magit
   :defer nil
