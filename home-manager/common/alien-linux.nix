583477e12652bae3f9d3865df8b93b12071c1fa9
George Macon <george@themacons.net>
2025-02-01 10:41:08 -0500

    A few more pinning cleanups

--- home-manager/common/alien-linux.nix
+++ home-manager/common/alien-linux.nix
@@ -1,6 +1,12 @@
 { sources, ... }:
 {
   nix = {
-    registry.nixpkgs.flake = sources.nixpkgs-stable;
+    registry.nixpkgs.to = {
+      type = "path";
+      path = sources.nixpkgs-stable;
+    };
+  };
+  home.sessionVariables = {
+    NIX_PATH = "nixpkgs=${sources.nixpkgs-stable}:home-manager=${sources.home-manager}";
   };
 }
--- nixos/common.nix
+++ nixos/common.nix
@@ -1,23 +1,26 @@
-{ pkgs, ... }:
+{ pkgs, sources, ... }:
 {
-  nix.package = pkgs.lix;
-  nix.settings = {
-    experimental-features = [
-      "nix-command"
-      "flakes"
-    ];
-    trusted-users = [
-      "root"
-      "@wheel"
-    ];
-    keep-outputs = true;
-    keep-derivations = true;
-    auto-optimise-store = true;
-  };
-  nix.gc = {
-    automatic = true;
-    dates = "weekly";
-    randomizedDelaySec = "1h";
-    options = "--delete-older-than 30d";
+  nixpkgs.flake.source = sources.nixpkgs-stable;
+  nix = {
+    package = pkgs.lix;
+    settings = {
+      experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
+      trusted-users = [
+        "root"
+        "@wheel"
+      ];
+      keep-outputs = true;
+      keep-derivations = true;
+      auto-optimise-store = true;
+    };
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      randomizedDelaySec = "1h";
+      options = "--delete-older-than 30d";
+    };
   };
 }
