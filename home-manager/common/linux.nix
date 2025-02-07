b1d18665d0815b3c081113dbc684ed02b908a349
George Macon <george.macon@gtri.gatech.edu>
2025-02-07 11:06:54 -0500

    Warn at login if not update recently

--- home-manager/common/linux.nix
+++ home-manager/common/linux.nix
@@ -1,10 +1,19 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 {
   targets.genericLinux.enable = true;
   programs.bash = {
     enable = true;
     bashrcExtra = ''
       umask 022
+    '';
+    profileExtra = ''
+      if [[ -e ${config.xdg.stateHome}/hm-activation-stamp ]]; then
+        activation_age=$(($(date +%s) - $(stat -c %Y -- ${config.xdg.stateHome}/hm-activation-stamp)))
+        if [[ $activation_age > 604800 ]]; then
+          echo "Home Manager last activated $(($activation_age / 86400)) days ago."
+        fi
+        unset activation_age
+      fi
     '';
   };
 
