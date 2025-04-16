e53c07d2105d4ebc20ca6efbbcf93a5357eeed65
George Macon <george@themacons.net>
2025-04-15 21:06:44 -0400

    [ar] Switch to KDE

--- argon/configuration.nix
+++ argon/configuration.nix
@@ -55,18 +55,11 @@
 
   # Secrets
 
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
-  # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
-
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "";
-  };
+  # Enable the KDE Desktop Environment.
+  services.displayManager.sddm.enable = true;
+  services.displayManager.sddm.wayland.enable = true;
+  services.desktopManager.plasma6.enable = true;
+  hardware.bluetooth.enable = true;
 
   # Enable CUPS to print documents.
   services.printing = {
