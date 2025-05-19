8f7891ffad4378c093112476817cbf1310f97ad0
George Macon <george.macon@gtri.gatech.edu>
2025-05-19 10:05:06 -0400

    [alien] Fix 1Password 2FA

--- home-manager/common/alien-linux.nix
+++ home-manager/common/alien-linux.nix
@@ -6,4 +6,9 @@
       path = sources.nixpkgs-stable;
     };
   };
+
+  # Force Nix-built applications to open things through the portal.
+  # This makes custom URL schemes work correctly
+  # when they are registered by non-Nix applications.
+  xdg.portal.xdgOpenUsePortal = true;
 }
