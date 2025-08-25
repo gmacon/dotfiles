01d1c3c78f246d80fddca82e54abad8a278650b9
George Macon <george.macon@gtri.gatech.edu>
2025-08-25 13:02:36 -0400

    Define Nix config in system-manager

--- system-manager/default.nix
+++ system-manager/default.nix
@@ -10,6 +10,19 @@ in
         config = {
           nixpkgs.hostPlatform = "x86_64-linux";
           system-graphics.enable = true;
+
+          nix.settings = {
+            experimental-features = [
+              "nix-command"
+              "flakes"
+            ];
+            trusted-users = [ "gmacon3" ];
+            keep-outputs = true;
+            keep-derivations = true;
+            auto-optimise-store = true;
+            substituters = [ "https://cache.lix.systems" ];
+            trusted-public-keys = [ "cache.lix.systems:aBnZUw8zA7H35Cz2RyKFVs3H4PlGTLawyY5KRbvJR8o=" ];
+          };
         };
       }
     ];
