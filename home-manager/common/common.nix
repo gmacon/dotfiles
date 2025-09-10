6ddf50e3740f498e4904ca29bd055c29994845da
George Macon <george.macon@gtri.gatech.edu>
2025-09-10 09:54:44 -0400

    Drop custom SSH crypto config
    Since modern OpenSSH has strong defaults.

--- home-manager/common/common.nix
+++ home-manager/common/common.nix
@@ -245,44 +245,6 @@ in
     extraConfig = ''
       ConnectTimeout 120
     '';
-    extraOptionOverrides = {
-      # Mozilla Cryptography Recommendations
-      HostKeyAlgorithms = builtins.concatStringsSep "," [
-        "ssh-ed25519-cert-v01@openssh.com"
-        "ssh-ed25519"
-        "ecdsa-sha2-nistp521-cert-v01@openssh.com"
-        "ecdsa-sha2-nistp384-cert-v01@openssh.com"
-        "ecdsa-sha2-nistp256-cert-v01@openssh.com"
-        "ecdsa-sha2-nistp521"
-        "ecdsa-sha2-nistp384"
-        "ecdsa-sha2-nistp256"
-        "ssh-rsa-cert-v01@openssh.com"
-        "ssh-rsa"
-      ];
-      KexAlgorithms = builtins.concatStringsSep "," [
-        "curve25519-sha256@libssh.org"
-        "ecdh-sha2-nistp521"
-        "ecdh-sha2-nistp384"
-        "ecdh-sha2-nistp256"
-        "diffie-hellman-group-exchange-sha256"
-      ];
-      MACs = builtins.concatStringsSep "," [
-        "hmac-sha2-512-etm@openssh.com"
-        "hmac-sha2-256-etm@openssh.com"
-        "umac-128-etm@openssh.com"
-        "hmac-sha2-512"
-        "hmac-sha2-256"
-        "umac-128@openssh.com"
-      ];
-      Ciphers = builtins.concatStringsSep "," [
-        "chacha20-poly1305@openssh.com"
-        "aes256-gcm@openssh.com"
-        "aes128-gcm@openssh.com"
-        "aes256-ctr"
-        "aes192-ctr"
-        "aes128-ctr"
-      ];
-    };
     includes = [ "host_*.config" ];
   };
 
